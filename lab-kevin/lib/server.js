d422db8d8c4351fbb38c48a3c0a67b65990330ba
Kevin Miller <kevin@driftabout.com>
2018-02-05 14:02:27 -0800

    npm init and install

--- lab-kevin/lib/server.js
+++ lab-kevin/lib/server.js
@@ -1,40 +1,48 @@
 'use strict';
 
+//app dependencies
 const express = require('express');
+const debug = require('debug');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const errorHandler = require('./error-handler');
+require('dotenv').config();
+const errorHandler = require('errorHandler');
 
+
+//App setup
 const app = express();
-app.use(cors);
+const router = express.Router() ;
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const router = app.Router();
 
-require('../router.route')(router);
+app.use(cors());
+app.use('./api/v1', router);
+require('./route/route_auth')(router);
+app.use('/*', (req, res) => errorHandler(new Error('Path Error: Requested path not found'), res));
 
-app.use('/api/v1/note', router);
-app.use('/*', (req, res) => errorHandler(new Error('Path Error: Path not found'), res));
 
 const server = module.exports = {};
 
+
 server.start = () => {
   return new Promise((resolve, reject) => {
-    if(server.isOn) return reject(new Error('Server already running'));
-    server.http = app.listen(process.env.PORT, () => {
-      console.log(`Listening on ${process.env.PORT}`);
-      server.isOn = true;
-      server.db = mongoose.connect(process.env.MONGO_URI)
-    });
-    resolve(server);
+    if(server.isOn) return reject (new Error('Server Error: Server is already running.')); 
+    server.isOn = true;
+    mongoose.connect(MONGODB_URI);
+    server.http = app.listen(PORT, (PORT) => console.log(`Server listening on ${PORT}`));
+    return resolve(server);
   });
 };
 
 server.stop = () => {
-  return new Promise( (resolve, reject) => {
-    if(!server.isOn) return reject(new Error('Server already stopped'));
-    console.log('Server shutting down');
-    server.isOn = false;
-    server.http.close();
-    server.db.disconnect();
+  return new Promise((resolve, reject) => {
+    if(!server.isOn) return reject (new Error('Server Error: Server is already stopped.')); 
+    server.http.close(() => {
+      server.isOn = false;
+      mongoose.disconnect();
+      console.log('Server has stopped listening');
+      return resolve(); 
+    });
   });
 };
