0aaccae504d06ab5f7473f7dbcad41dfeabfc319
Mitchell M <mitchellm1618@gmail.com>
2018-02-05 15:02:49 -0800

    saving mocks testing before switching to without mocks

--- lab-mitchell/__test__/integration/auth-post.test.js
+++ lab-mitchell/__test__/integration/auth-post.test.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const mock = require('../lib/mocks');
+const superagent = require('superagent');
+const server = require('../../lib/server');
+
+describe('#auth-post.test', function() {
+  console.log(mock.auth.createOne());
+  describe('valid input/output', () => {
+    beforeAll(() => {
+
+    });
+  });
+  describe('invalid input/output', () => {
+
+  });
+});
