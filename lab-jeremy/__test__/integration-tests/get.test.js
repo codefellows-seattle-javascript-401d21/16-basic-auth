97c5fb4ce55d191495cf3a8c24c644201807a402
Jeremy <jpjazzy@hotmail.com>
2018-02-05 22:10:39 -0800

    added testing content and mock data

---
+++ lab-jeremy/__test__/integration-tests/get.test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const server = require('../../lib/server.js');
+const superagent = require('superagent');
+const mock = require('../lib/mock.js');
+require('jest');
+
+let api = ':4000/api/v1';
+
+describe('GET :4000/api/v1/signin', function() {
+  beforeAll(server.start);
+  afterAll(server.stop);
+  afterAll(mock.auth.removeAll);
+
+
+  describe('Valid req/res', () => {
+    beforeAll(() => {
+      return mock.auth.createOne()
+        .then(data => {
+          this.authData = data;
+          return superagent.get(`${api}/signin`)
+            .auth(this.authData.auth.username, data.password)
+            .then(res => this.response = res);
+        });
+    });
+    it('should return a status of 200', () => {
+      expect(this.response.status).toBe(200);
+    });
+    it('should return a token', () => {
+      expect(this.authData).toHaveProperty('token');
+    });
+  });
+  describe('Invalid req/res', () => {
+    it('should return a status 400 given no request body', () => {
+      return superagent.post(`${api}/signup`)
+        .send({
+          username: 'me',
+          password: 'password',
+          email: 'hi',
+        })
+        .then((res) => {
+          this.response = res;
+          return superagent.get(`${api}/signin`)
+            .auth('me', 'notmypassword')
+            .catch(err => {
+              this.errRes = err;
+              expect(this.errRes.status).toEqual(401);
+            });
+        });
+    });
+    it('should return a status 404 on an invalid path', () => {
+      return superagent.get(`${api}/note`)
+        .catch(err => expect(err.status).toEqual(404));
+    });
+  });
+});
