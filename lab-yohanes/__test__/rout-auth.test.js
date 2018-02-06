abf1aef05d098a1b4e938c915ff62f0bf0fb3851
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-02-05 23:22:07 -0800

    having trouble running my nodemon. database not working. Getting rest and resuming tomorrow

---
+++ lab-yohanes/__test__/rout-auth.test.js
@@ -0,0 +1,11 @@
+'use strict'; 
+
+const route = require('../route/route-auth.js');
+
+describe('Router Authorization Module', function () {
+  describe('#res.status(201)', function () {
+    it('should give us a res.status 201', function () {
+      expect(user.generatePasswordHash(pw)).tobe(something)
+    });
+  });
+});
