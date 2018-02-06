4e8f17b77f9579f9619279aa70d2b368c2148051
billodell <billodell5@gmail.com>
2018-02-05 16:14:21 -0800

    tests working

---
+++ lab-bill/__test__/lib/mocks.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const faker = require('faker');
+const Auth = require('../../model/auth');
+const errorHandler = require('../../lib/error-handler');
+const mock = module.exports = {};
+
+
+mock.auth = {};
+
+mock.auth.createOne = () => new Auth({ 
+  username: faker.hacker.adjective(),
+  password: faker.hacker.adjective(),
+  email: faker.hacker.adjective(),
+}).save();
+
+mock.auth.removeAll = () => Promise.all([Auth.remove()]);
