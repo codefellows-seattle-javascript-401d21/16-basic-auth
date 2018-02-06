c04c4bbcfda6de9ab5f06c91a07bea27161f6695
Mitchell M <mitchellm1618@gmail.com>
2018-02-05 18:44:27 -0800

    auth-post valid/invalid tests passing

--- lab-mitchell/__test__/integration/auth-post.test.js
+++ lab-mitchell/__test__/integration/auth-post.test.js
@@ -11,7 +11,7 @@ describe('#auth-post /api/v1/signup', function () {
   beforeAll(() => this.base = `:${process.env.PORT}/api/v1/signup`);
   beforeAll(server.start);
   afterAll(server.stop);
-  // afterEach(mock.auth.removeAll);
+  afterEach(mock.auth.removeAll);
 
   describe('valid input/output', () => {
     // beforeAll(() => {
@@ -25,7 +25,7 @@ describe('#auth-post /api/v1/signup', function () {
     //       };
 
     //       return superagent.post(`${this.base}`)
-    //         .send(this.mockAuth)
+    //         .send({username: this.mockAuth.username, password: this.mockAuth.password, email: this.mockAuth.email})
     //         .then(res => this.response = res)
     //         .catch(err => errorHandler(err));
     //     });
@@ -40,14 +40,60 @@ describe('#auth-post /api/v1/signup', function () {
     
     it('should return a response status of 201', () => {
       // console.log('mockauth:',this.mockAuth);
-      console.log('this.reponse.body:', this.response);
+      // console.log('this.reponse.body:', this.response);
       expect(this.response.status).toBe(201);
+      expect(this.response).toHaveProperty('headers');
+    });
+    it('should return a response with headers property', () => {
+      expect(this.response).toHaveProperty('headers');
+    });
+    it('should POST a new Auth and respond with a hashed string', () => {
+      expect(this.response.body).toContain('eyJhbGciOiJIUzI1NiJ9.');
     });
   });
 
-
-
   describe('invalid input/output', () => {
-
+    it('should return an error when sending malformed data', () => {
+      return superagent.post(`${this.base}`)
+        .send()
+        .catch(err => {
+          expect(err).not.toBeNull();
+        });
+    });
+    it('should return status 401 without required properties sent in request', () => {
+      return superagent.post(`${this.base}`)
+        .send()
+        .catch(err => {
+          expect(err.status).toBe(401);
+        });
+    });
+    it('should return status 401 without required properties sent in request, with error message unauthorized', () => {
+      return superagent.post(`${this.base}`)
+        .send()
+        .catch(err => {
+          expect(err.message).toContain('Unauthorized');
+        });
+    });
+    it('should return an error with bad path', () => {
+      return superagent.post(`${this.base}/badpath`)
+        .send()
+        .catch(err => {
+          expect(err).not.toBeNull();
+        });
+    });
+    it('should return status 404 with bad path', () => {
+      return superagent.post(`${this.base}/badpath`)
+        .send()
+        .catch(err => {
+          expect(err.status).toBe(404);
+        });
+    });
+    it('should return status 404 with bad path, with error message Not Found', () => {
+      return superagent.post(`${this.base}/badpath`)
+        .send()
+        .catch(err => {
+          expect(err.message).toContain('Not Found');
+        });
+    });
   });
 });
