8970670c379dcd2b15c2b41e80c128d9b11086d1
strawbee <yuejiao.hou@gmail.com>
2018-02-11 20:36:43 -0800

    tests done

--- lab-joy/test/integration-tests/photo-get.test.js
+++ lab-joy/test/integration-tests/photo-get.test.js
@@ -7,7 +7,7 @@ const server = require('../../lib/server');
 const mocks = require('../lib/mocks');
 const path = `:${process.env.PORT}/api/v1/photo`;
 
-describe('POST /api/v1/photo', () => {
+describe('GET /api/v1/photo', () => {
     beforeAll(server.start);
     beforeAll(() => mocks.photo.createOne().then(data => this.mock = data));
     afterAll(server.stop);
@@ -15,29 +15,32 @@ describe('POST /api/v1/photo', () => {
     afterAll(mocks.note.removeAll);
     afterAll(mocks.photo.removeAll);
 
-    it('should return status 201 for valid post', () => {
-        expect(this.mock.photo.status).toBe(201);
-    });
-
-    it('should return status 401 if given an invalid token', () => {
-        return superagent.post(path)
-            .set('Authorization', `Bearer cats`)
-            .field('name', faker.internet.domainWord())
-            .field('desc', faker.random.words(15))
-            .field('noteId', this.mock.note._id.toString())
-            .attach('image', `${__dirname}/../snowdrop.jpg`)
-            .catch(err => expect(err.status).toBe(401));
-    });
-
-    it('should return status 400 given an invalid body', () => {
-        return mocks.note.createOne()
+    it('should return status 200 for valid get with an ID', () => {
+        return superagent.get(`${path}/${this.mock.photo.body._id}`)
+            .set('Authorization', `Bearer ${this.mock.token}`)
             .then(res => {
-                return superagent.post(path)
-                    .set('Authorization', `Bearer ${res.token}`)
-                    .send()
-                    .catch(err => {
-                        expect(err.status).toBe(400);
-                    });
+                expect(res.status).toBe(200);
+                expect(res.body._id).toBe(this.mock.photo.body._id);
             });
+    });
+
+    it('should return status 200 for valid get all', () => {
+        return superagent.get(path)
+            .set('Authorization', `Bearer ${this.mock.token}`)
+            .then(res => {
+                expect(res.body).toContain(this.mock.photo.body._id);
+            });
+    });
+
+    it('should return status 404 for invalid get request path', () => {
+        return superagent.get(`${path}/cats`)
+            .set('Authorization', `Bearer ${this.mock.token}`)
+            .catch(err => expect(err.status).toBe(404));
+    });
+
+    it('should return status 401 for invalid token', () => {
+        return superagent.get(`${path}/${this.mock.photo.body._id}`)
+            .set('Authorization', `Bearer cats`)
+            .catch(err => expect(err.status).toBe(401));
     });
 });
