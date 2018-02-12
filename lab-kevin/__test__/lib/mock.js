480a04d8d602f3d9c206637471f8409bd3be04f6
Kevin Miller <kevin@driftabout.com>
2018-02-11 18:55:34 -0800

    finished adding tests

--- lab-kevin/__test__/lib/mock.js
+++ lab-kevin/__test__/lib/mock.js
@@ -4,28 +4,39 @@ const faker = require('faker');
 const Auth = require('../../model/auth');
 const debug = require('debug')('http:mock');
 
+debug('mocks');
+
 const mock = module.exports = {};
 
+mock.auth = {};
 
-mock.user = {
-  username: `${faker.name.prefix()}${faker.hacker.adjective()}`.replace(/[.\s]/, ''),
-  email: `${faker.internet.email()}`,
-  password:`${faker.hacker.adjective()}${faker.hacker.noun()}`.replace(/[.\s]/, ''),
-
+mock.new_user = () => {
+  return {
+    username: `${faker.name.prefix()}${faker.hacker.adjective()}`.replace(/[.\s]/, ''),
+    email: `${faker.internet.email()}`,
+    password:`${faker.hacker.adjective()}${faker.hacker.noun()}`.replace(/[.\s]/, ''),
+  };
 };
 
-mock.createUser = () => {
-  let userCreds = mock.user;
-  debug('mockuseer', mock.user);
-  let pswd = userCreds.password;
-  let newUser = new Auth({username:userCreds.username, email:userCreds.email});
-  return newUser.createHashedpassword(pswd)
+mock.auth.createUser = () => {
+  let auth_data = {};
+  mock.user = mock.new_user();
+  debug('mock.user', mock.user);
+  auth_data.password = mock.user.password;
+  let newUser = Auth({username: mock.user.username, email: mock.user.email});
+  return newUser.createHashedpassword(auth_data.password)
     .then(() => newUser.save())
     .then(() => newUser.createToken())
-    .catch(console.err);
+    .then(token =>{
+      auth_data.user = newUser;
+      auth_data.user_token = token;
+      return auth_data;
+    })
+    .catch(err => err);
 };
 
 mock.removeUsers = () => Promise.all([Auth.remove()]); 
+
 
 
 
